eb1a16b70e91e61f061b7ae4e30a35a6b78487cb
Pravin Kumavat <kmwtpk95@gmail.com>
2026-07-15 08:58:18 +0530

    Update IF statement examples and syntax

--- 01_Python/IF_Statement.py
+++ 01_Python/IF_Statement.py
@@ -12,73 +12,73 @@
 
 #* WAP to print the square of a number only if it is even.
 
-# n = int(input('Enter the number : '))
-# if n%2 == 0:
-#     print(n*n)
+n = int(input('Enter the number : '))
+if n%2 == 0:
+ print(n)
 
 
 
 #* WAP to check whether the character is a vowel or not.
 
-# n = input('Enter the Character : ')
-# if n in 'aeiouAEIOU':
-#     print(f'{n} is a vowel character')
+n = input('Enter the Character : ')
+if n in 'aeiouAEIOU':
+ print(f'{n} is a vowel character')
 
 
 
 #* WAP to print the ASCII value of a character only if it is uppercase.
 
-# n = input('Enter the Character : ')
-# if 'A'<= n <= 'Z':
-#     print(ord(n))
+n = input('Enter the Character : ')
+if 'A'<= n <= 'Z':
+  print(ord(n))
 
 
 
 #* WAP to print the cube of a number only if it is divisible by 9 or 6.
 
-# n = int(input('Enter the number : '))
-# if 9%n == 0 and 6%n == 0:
-#     print(n*n) 
+n = int(input('Enter the number : '))
+if 9%n == 0 and 6%n == 0:
+ print(n*n) 
 
 
 
 #* WAP to check whether the given integer is a 3-digit number.
 
-# n = int(input('Enter the number : '))
-# if 99 < n < 1000:
-#     print(f'{n} is a three digit number')
+n = int(input('Enter the number : '))
+if 99 < n < 1000:
+ print(f'{n} is a three digit number')
 
 
 
 #* WAP to check whether the last digit of a given number is 5.
 
-# n = int(input('Enter the number : '))
-# if n%10 == 5:
-#     print(f'{n} have last digit of a given number is 5')
+n = int(input('Enter the number : '))
+if n%10 == 5:
+ print(f'{n} have last digit of a given number is 5')
 
 
 
 #* WAP to check whether the given data is float.
 
-# n = eval(input('Enter the data : '))
-# if type(n) == float:
-#     print(f'{n} having float data type')
+n = eval(input('Enter the data : '))
+if type(n) == float:
+ print(f'{n} having float data type')
 
 
 
 #* WAP to check whether the data is single value data.
 
-# n = eval(input('Enter the data : '))
-# if type(n) in (int, float, bool, complex):
-#     print(f'{n} having single value data type')
+n = eval(input('Enter the data : '))
+if type(n) in (int, float, bool, complex):
+ print(f'{n} having single value data type')
 
 
 
 #* WAP to check whether the given character is a digit or not.
 
-# n = eval(input('Enter the data : '))
-# if 0 <= n <= 9:
-#     print(f'{n} is a digit')
+n = eval(input('Enter the data : '))
+if 0 <= n <= 9:
+ print(f'{n} is a digit')
 
 
 
